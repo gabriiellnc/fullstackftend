bfe688e862f4c87e41b11a51945b88a128ed63d9
Gabriel Nunes <you@example.com>
2020-06-08 23:24:12 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'http://localhost:3002/api'
+    baseURL : 'https://fullstackbkend.herokuapp.com/api'
 });
 
 export default api;
